ed9dd5acd4a7b048b6099ac8a7c91e7cd0fa9066
Robart Petrov <75201368+Robarto97@users.noreply.github.com>
2021-04-03 20:36:25 +0300

    Add files via upload

---
+++ RaidPuller/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,342 @@
+﻿namespace RaidPuller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.OpenAncient = new System.Windows.Forms.Button();
+            this.RareBox = new System.Windows.Forms.ListBox();
+            this.EpicBox = new System.Windows.Forms.ListBox();
+            this.LegoBox = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.OpenSacred = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            this.OpenVoid = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.VoidRareBox = new System.Windows.Forms.ListBox();
+            this.VoidEpicBox = new System.Windows.Forms.ListBox();
+            this.VoidLegoBox = new System.Windows.Forms.ListBox();
+            this.imageListColorRare = new System.Windows.Forms.ImageList(this.components);
+            this.imageListColorEpic = new System.Windows.Forms.ImageList(this.components);
+            this.imageListColorLego = new System.Windows.Forms.ImageList(this.components);
+            this.PictureBox = new System.Windows.Forms.PictureBox();
+            this.Result = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ancient";
+            // 
+            // OpenAncient
+            // 
+            this.OpenAncient.Location = new System.Drawing.Point(94, 30);
+            this.OpenAncient.Name = "OpenAncient";
+            this.OpenAncient.Size = new System.Drawing.Size(75, 23);
+            this.OpenAncient.TabIndex = 6;
+            this.OpenAncient.Text = "OpenA";
+            this.OpenAncient.UseVisualStyleBackColor = true;
+            this.OpenAncient.Click += new System.EventHandler(this.OpenAncient_Click);
+            // 
+            // RareBox
+            // 
+            this.RareBox.FormattingEnabled = true;
+            this.RareBox.Items.AddRange(new object[] {
+            "Bombardier",
+            "Cataphract",
+            "Conquerer",
+            "Courtier",
+            "Crossbowman",
+            "Grandmaster",
+            "Halberdier",
+            "Myrmidon",
+            "Preserver",
+            "Quaestor",
+            "Valerie"});
+            this.RareBox.Location = new System.Drawing.Point(1028, 12);
+            this.RareBox.Name = "RareBox";
+            this.RareBox.Size = new System.Drawing.Size(120, 30);
+            this.RareBox.TabIndex = 13;
+            this.RareBox.Visible = false;
+            // 
+            // EpicBox
+            // 
+            this.EpicBox.FormattingEnabled = true;
+            this.EpicBox.Items.AddRange(new object[] {
+            "Alaric the Hooded",
+            "Archmage Hellmut",
+            "Chancellor Yasmin",
+            "Gerhard the Stone",
+            "Hordin",
+            "Knight-Errant",
+            "Lordly Legionary",
+            "Masker Fearmonger",
+            "Oathbound",
+            "Seneschal",
+            "Stag Knight"});
+            this.EpicBox.Location = new System.Drawing.Point(1028, 48);
+            this.EpicBox.Name = "EpicBox";
+            this.EpicBox.Size = new System.Drawing.Size(120, 30);
+            this.EpicBox.TabIndex = 14;
+            this.EpicBox.Visible = false;
+            // 
+            // LegoBox
+            // 
+            this.LegoBox.FormattingEnabled = true;
+            this.LegoBox.Items.AddRange(new object[] {
+            "Black Knight",
+            "Cillian the Lucky",
+            "Lord Champfort",
+            "Lugan the Steadfast",
+            "Minaya",
+            "Richtoff the Bold",
+            "Septimus",
+            "Sethallia"});
+            this.LegoBox.Location = new System.Drawing.Point(1028, 85);
+            this.LegoBox.Name = "LegoBox";
+            this.LegoBox.Size = new System.Drawing.Size(120, 30);
+            this.LegoBox.TabIndex = 15;
+            this.LegoBox.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Sacred";
+            // 
+            // OpenSacred
+            // 
+            this.OpenSacred.Location = new System.Drawing.Point(94, 134);
+            this.OpenSacred.Name = "OpenSacred";
+            this.OpenSacred.Size = new System.Drawing.Size(75, 23);
+            this.OpenSacred.TabIndex = 17;
+            this.OpenSacred.Text = "OpenS";
+            this.OpenSacred.UseVisualStyleBackColor = true;
+            this.OpenSacred.Click += new System.EventHandler(this.OpenSacred_Click);
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(94, 186);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(75, 23);
+            this.Clear.TabIndex = 20;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // OpenVoid
+            // 
+            this.OpenVoid.Location = new System.Drawing.Point(94, 80);
+            this.OpenVoid.Name = "OpenVoid";
+            this.OpenVoid.Size = new System.Drawing.Size(75, 23);
+            this.OpenVoid.TabIndex = 21;
+            this.OpenVoid.Text = "OpenV";
+            this.OpenVoid.UseVisualStyleBackColor = true;
+            this.OpenVoid.Click += new System.EventHandler(this.OpenVoid_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "Void";
+            // 
+            // VoidRareBox
+            // 
+            this.VoidRareBox.FormattingEnabled = true;
+            this.VoidRareBox.Items.AddRange(new object[] {
+            "Coldheart",
+            "Bellower",
+            "Fellhound",
+            "Dilgol",
+            "Pain Keeper"});
+            this.VoidRareBox.Location = new System.Drawing.Point(1028, 123);
+            this.VoidRareBox.Name = "VoidRareBox";
+            this.VoidRareBox.Size = new System.Drawing.Size(120, 30);
+            this.VoidRareBox.TabIndex = 24;
+            this.VoidRareBox.Visible = false;
+            // 
+            // VoidEpicBox
+            // 
+            this.VoidEpicBox.FormattingEnabled = true;
+            this.VoidEpicBox.Items.AddRange(new object[] {
+            "Madame Serris",
+            "Peydma",
+            "Umbrall",
+            "Psylar",
+            "Golden Reaper"});
+            this.VoidEpicBox.Location = new System.Drawing.Point(1028, 159);
+            this.VoidEpicBox.Name = "VoidEpicBox";
+            this.VoidEpicBox.Size = new System.Drawing.Size(120, 30);
+            this.VoidEpicBox.TabIndex = 25;
+            this.VoidEpicBox.Visible = false;
+            // 
+            // VoidLegoBox
+            // 
+            this.VoidLegoBox.FormattingEnabled = true;
+            this.VoidLegoBox.Items.AddRange(new object[] {
+            "Arbiter",
+            "Krisk",
+            "Raglin",
+            "Turvold",
+            "Venus"});
+            this.VoidLegoBox.Location = new System.Drawing.Point(1028, 195);
+            this.VoidLegoBox.Name = "VoidLegoBox";
+            this.VoidLegoBox.Size = new System.Drawing.Size(120, 30);
+            this.VoidLegoBox.TabIndex = 26;
+            this.VoidLegoBox.Visible = false;
+            // 
+            // imageListColorRare
+            // 
+            this.imageListColorRare.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageListColorRare.ImageStream")));
+            this.imageListColorRare.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageListColorRare.Images.SetKeyName(0, "Bombardier.jpg");
+            this.imageListColorRare.Images.SetKeyName(1, "Cataphract.jpg");
+            this.imageListColorRare.Images.SetKeyName(2, "Conquerer.jpg");
+            this.imageListColorRare.Images.SetKeyName(3, "Courtier.jpg");
+            this.imageListColorRare.Images.SetKeyName(4, "Crossbowman.jpg");
+            this.imageListColorRare.Images.SetKeyName(5, "Grandmaster.jpg");
+            this.imageListColorRare.Images.SetKeyName(6, "Halberdier.jpg");
+            this.imageListColorRare.Images.SetKeyName(7, "Myrmidon.jpg");
+            this.imageListColorRare.Images.SetKeyName(8, "Preserver.jpg");
+            this.imageListColorRare.Images.SetKeyName(9, "Quaestor.jpg");
+            this.imageListColorRare.Images.SetKeyName(10, "Valerie.jpg");
+            // 
+            // imageListColorEpic
+            // 
+            this.imageListColorEpic.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageListColorEpic.ImageStream")));
+            this.imageListColorEpic.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageListColorEpic.Images.SetKeyName(0, "Alaric the Hooded.jpg");
+            this.imageListColorEpic.Images.SetKeyName(1, "Archmage Hellmut.jpg");
+            this.imageListColorEpic.Images.SetKeyName(2, "Chancellor Yasmin.jpg");
+            this.imageListColorEpic.Images.SetKeyName(3, "Gerhard the Stone.jpg");
+            this.imageListColorEpic.Images.SetKeyName(4, "Hordin.jpg");
+            this.imageListColorEpic.Images.SetKeyName(5, "Knight-Errant.jpg");
+            this.imageListColorEpic.Images.SetKeyName(6, "Lordly Legionary.jpg");
+            this.imageListColorEpic.Images.SetKeyName(7, "Masked Fearmonger.jpg");
+            this.imageListColorEpic.Images.SetKeyName(8, "Oathbound.jpg");
+            this.imageListColorEpic.Images.SetKeyName(9, "Seneschal.jpg");
+            this.imageListColorEpic.Images.SetKeyName(10, "Stag Knight.jpg");
+            // 
+            // imageListColorLego
+            // 
+            this.imageListColorLego.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageListColorLego.ImageStream")));
+            this.imageListColorLego.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageListColorLego.Images.SetKeyName(0, "Black Knight.jpg");
+            this.imageListColorLego.Images.SetKeyName(1, "Cillian the Lucky.jpg");
+            this.imageListColorLego.Images.SetKeyName(2, "Lord Champfort.jpg");
+            this.imageListColorLego.Images.SetKeyName(3, "Lugan the Steadfast.jpg");
+            this.imageListColorLego.Images.SetKeyName(4, "Minaya.jpg");
+            this.imageListColorLego.Images.SetKeyName(5, "Richtoff the Bold.jpg");
+            this.imageListColorLego.Images.SetKeyName(6, "Septimus.jpg");
+            this.imageListColorLego.Images.SetKeyName(7, "Sethallia.jpg");
+            // 
+            // PictureBox
+            // 
+            this.PictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.PictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PictureBox.Location = new System.Drawing.Point(0, 0);
+            this.PictureBox.Name = "PictureBox";
+            this.PictureBox.Size = new System.Drawing.Size(984, 523);
+            this.PictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PictureBox.TabIndex = 28;
+            this.PictureBox.TabStop = false;
+            // 
+            // Result
+            // 
+            this.Result.AutoSize = true;
+            this.Result.Location = new System.Drawing.Point(579, 80);
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(28, 13);
+            this.Result.TabIndex = 27;
+            this.Result.Text = "Test";
+            this.Result.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 523);
+            this.Controls.Add(this.PictureBox);
+            this.Controls.Add(this.Result);
+            this.Controls.Add(this.VoidLegoBox);
+            this.Controls.Add(this.VoidEpicBox);
+            this.Controls.Add(this.VoidRareBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.OpenVoid);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.OpenSacred);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.LegoBox);
+            this.Controls.Add(this.EpicBox);
+            this.Controls.Add(this.RareBox);
+            this.Controls.Add(this.OpenAncient);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button OpenAncient;
+        private System.Windows.Forms.ListBox RareBox;
+        private System.Windows.Forms.ListBox EpicBox;
+        private System.Windows.Forms.ListBox LegoBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button OpenSacred;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.Button OpenVoid;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox VoidRareBox;
+        private System.Windows.Forms.ListBox VoidEpicBox;
+        private System.Windows.Forms.ListBox VoidLegoBox;
+        private System.Windows.Forms.ImageList imageListColorEpic;
+        private System.Windows.Forms.ImageList imageListColorLego;
+        public System.Windows.Forms.PictureBox PictureBox;
+        private System.Windows.Forms.ImageList imageListColorRare;
+        private System.Windows.Forms.Label Result;
+    }
+}
+
---
+++ RaidPuller/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RaidPuller
+{
+    public partial class Form1 : Form
+    {
+        List<Image> imagesRare = new List<Image>();
+        List<Image> imagesEpic = new List<Image>();
+        List<Image> imagesLego = new List<Image>();
+        
+        
+        public Form1()
+        {
+            InitializeComponent();
+            label1.BringToFront();
+            label2.BringToFront();
+            label3.BringToFront();
+            OpenAncient.BringToFront();
+            OpenVoid.BringToFront();
+            OpenSacred.BringToFront();
+            Clear.BringToFront();
+            Result.BringToFront();
+        }
+        private void OpenAncient_Click(object sender, EventArgs e)
+        {
+            var random = new Random();
+            int chance = random.Next(0,100);
+            int rares = RareBox.Items.Count;
+            int epics = EpicBox.Items.Count;
+            int legos = LegoBox.Items.Count;
+            try
+            {
+                if (chance > 7)
+                {
+                    imagesRare.Add(Image.FromFile(@"C:\Users\My\Desktop\Pictures\Banner lords\Rare\Bombardier.jpg"));
+                    imagesRare.Add(Image.FromFile(@"C:\Users\My\Desktop\Pictures\Banner lords\Rare\Cataphract.jpg"));
+                    imagesRare.Add(Image.FromFile(@"C:\Users\My\Desktop\Pictures\Banner lords\Rare\Conquerer.jpg"));
+                    imagesRare.Add(Image.FromFile(@"C:\Users\My\Desktop\Pictures\Banner lords\Rare\Courtier.jpg"));
+                    imagesRare.Add(Image.FromFile(@"C:\Users\My\Desktop\Pictures\Banner lords\Rare\Crossbowman.jpg"));
+                    imagesRare.Add(Image.FromFile(@"C:\Users\My\Desktop\Pictures\Banner lords\Rare\Grandmaster.jpg"));
+                    imagesRare.Add(Image.FromFile(@"C:\Users\My\Desktop\Pictures\Banner lords\Rare\Halberdier.jpg"));
+                    imagesRare.Add(Image.FromFile(@"C:\Users\My\Desktop\Pictures\Banner lords\Rare\Myrmidon.jpg"));
+                    imagesRare.Add(Image.FromFile(@"C:\Users\My\Desktop\Pictures\Banner lords\Rare\Preserver.jpg"));
+                    imagesRare.Add(Image.FromFile(@"C:\Users\My\Desktop\Pictures\Banner lords\Rare\Quaestor.jpg"));
+                    imagesRare.Add(Image.FromFile(@"C:\Users\My\Desktop\Pictures\Banner lords\Rare\Valerie.jpg"));
+                    int rare = random.Next(0, rares);
+                    Result.Text = RareBox.Items[rare].ToString() + "\n Got rare!";
+                    PictureBox.Image = imagesRare[rare];
+                    imagesRare.Clear();
+                }
+
+                if (chance > 1 & chance <= 7)
+                {
+                    imagesEpic.Add(Image.FromFile(@"C:\Users\My\Desktop\Pictures\Banner lords\Epic\Alaric the Hooded.jpg"));
+                    imagesEpic.Add(Image.FromFile(@"C:\Users\My\Desktop\Pictures\Banner lords\Epic\Archmage Hellmut.jpg"));
+                    imagesEpic.Add(Image.FromFile(@"C:\Users\My\Desktop\Pictures\Banner lords\Epic\Chancellor Yasmin.jpg"));
+                    imagesEpic.Add(Image.FromFile(@"C:\Users\My\Desktop\Pictures\Banner lords\Epic\Gerhard the Stone.jpg"));
+                    imagesEpic.Add(Image.FromFile(@"C:\Users\My\Desktop\Pictures\Banner lords\Epic\Hordin.jpg"));
+                    imagesEpic.Add(Image.FromFile(@"C:\Users\My\Desktop\Pictures\Banner lords\Epic\Knight-Errant.jpg"));
+                    imagesEpic.Add(Image.FromFile(@"C:\Users\My\Desktop\Pictures\Banner lords\Epic\Lordly Legionary.jpg"));
+                    imagesEpic.Add(Image.FromFile(@"C:\Users\My\Desktop\Pictures\Banner lords\Epic\Masked Fearmonger.jpg"));
+                    imagesEpic.Add(Image.FromFile(@"C:\Users\My\Desktop\Pictures\Banner lords\Epic\Oathbound.jpg"));
+                    imagesEpic.Add(Image.FromFile(@"C:\Users\My\Desktop\Pictures\Banner lords\Epic\Seneschal.jpg"));
+                    imagesEpic.Add(Image.FromFile(@"C:\Users\My\Desktop\Pictures\Banner lords\Epic\Stag Knight.jpg"));
+                    int epic = random.Next(0, epics);
+                    Result.Text = EpicBox.Items[epic].ToString() + "\n Got epic!";
+                    PictureBox.Image = imagesEpic[epic];
+                    imagesEpic.Clear();
+                }
+
+                if (chance <= 1)
+                {
+                    imagesLego.Add(Image.FromFile(@"C:\Users\My\Desktop\Pictures\Banner lords\Legendary\Black Knight.jpg"));
+                    imagesLego.Add(Image.FromFile(@"C:\Users\My\Desktop\Pictures\Banner lords\Legendary\Cillian the Lucky.jpg"));
+                    imagesLego.Add(Image.FromFile(@"C:\Users\My\Desktop\Pictures\Banner lords\Legendary\Lord Champfort.jpg"));
+                    imagesLego.Add(Image.FromFile(@"C:\Users\My\Desktop\Pictures\Banner lords\Legendary\Lugan the Steadfast.jpg"));
+                    imagesLego.Add(Image.FromFile(@"C:\Users\My\Desktop\Pictures\Banner lords\Legendary\Minaya.jpg"));
+                    imagesLego.Add(Image.FromFile(@"C:\Users\My\Desktop\Pictures\Banner lords\Legendary\Richtoff the Bold.jpg"));
+                    imagesLego.Add(Image.FromFile(@"C:\Users\My\Desktop\Pictures\Banner lords\Legendary\Septimus.jpg"));
+                    imagesLego.Add(Image.FromFile(@"C:\Users\My\Desktop\Pictures\Banner lords\Legendary\Sethallia.jpg"));
+                    int lego = random.Next(0, legos);
+                    Result.Text = LegoBox.Items[lego].ToString() + "\n Got lego!";
+                    PictureBox.Image = imagesLego[lego];
+                    imagesLego.Clear();
+                }
+            }
+            catch (OutOfMemoryException)
+            {
+                Console.WriteLine("No memory!");
+            }
+        }
+        private void OpenSacred_Click(object sender, EventArgs e)
+        {
+            var random = new Random();
+            int chance = random.Next(0, 100);
+            int epics = EpicBox.Items.Count;
+            int legos = LegoBox.Items.Count;
+
+            if (chance > 6) {
+                int epic = random.Next(1, epics);
+                Result.Text = EpicBox.Items[epic].ToString() + "\n Got epic!";
+            }
+
+            if (chance <= 6) {
+                int lego = random.Next(1, legos);
+                Result.Text = LegoBox.Items[lego].ToString() + "\n Got lego!";
+            }
+        }
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            Result.Text = "";
+            PictureBox.Image = null;
+        }
+        private void OpenVoid_Click(object sender, EventArgs e)
+        {
+            var random = new Random();
+            int chance = random.Next(0, 100);
+            int rares = VoidRareBox.Items.Count;
+            int epics = VoidEpicBox.Items.Count;
+            int legos = VoidLegoBox.Items.Count;
+
+            if (chance > 7) {
+                int rare = random.Next(1, rares);
+                Result.Text = VoidRareBox.Items[rare].ToString() + "\n Got rare!";
+            }
+
+            if (chance > 1 & chance <=7) {
+                int epic = random.Next(1, epics);
+                Result.Text = VoidEpicBox.Items[epic].ToString() + "\n Got epic!";
+            }
+
+            if (chance == 1) {
+                int lego = random.Next(1, legos);
+                Result.Text = VoidLegoBox.Items[lego].ToString() + "\n Got lego!";
+            }
+        }
+
+
+        public Image images { get; set; }
+    }
+}
